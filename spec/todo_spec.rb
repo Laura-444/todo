fac3be7dd67ff134a2dc6f520d8bbdb57b4ba038
Laura Valladares <lauvagar41012@gmail.com>
2025-06-03 22:21:57 -0600

    test: update specs to use symbols and fix initialization

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -1,6 +1,23 @@
 RSpec.describe Todo do
-  let(:storage) { InMemoryStorage.new }
+  let(:storage) { MemoryStorage.new }
   let(:todo) { Todo.new storage }
+
+  let :testTasks do
+    [
+      { id: '123',
+        title: 'Call the vet',
+        description: 'appointment for vaccines',
+        done: false,  },
+      { id: '567',
+        title: 'Go to the store',
+        description: 'buy vegetables',
+        done: false, },
+    ]
+  end
+
+  before do
+    storage.write testTasks
+  end
 
   describe '.list_tasks' do
     let(:result) { todo.list_tasks }
@@ -11,13 +28,6 @@ RSpec.describe Todo do
   end
 
   describe '.find_task' do
-    before do
-      storage.write([
-        { 'id' => '123', 'title' => 'Call the vet', 'description' => 'appointment for vaccines', 'done' => false },
-        { 'id' => '567', 'title' => 'Go to the store', 'description' => 'buy vegetables', 'done' => false },
-      ])
-    end
-
     let(:result) { todo.find_task id }
 
     context 'When ID exist' do
@@ -25,7 +35,8 @@ RSpec.describe Todo do
 
       it 'finds the desired task' do
         expect(result).to be_a(Hash)
-        expect(result['id']).to eq(id)
+        expect(result[:id]).to eq(id)
+        expect(result[:title]).to eq('Call the vet')
       end
     end
 
@@ -39,25 +50,12 @@ RSpec.describe Todo do
   end
 
   describe '.delete_task' do
-    before do
-      storage.write([
-        { 'id' => '123',
-          'title' => 'Call the vet',
-          'description' => 'appointment for vaccines',
-          'done' => false,  },
-        { 'id' => '567',
-          'title' => 'Go to the store',
-          'description' => 'buy vegetables',
-          'done' => false, },
-      ])
-    end
-
     let(:id) { '123' }
     let(:result) { todo.delete_task id }
 
     it 'retrieves the deleted task' do
       expect(result).to be_a(Hash)
-      expect(result['id']).to eq(id)
+      expect(result[:id]).to eq(id)
     end
 
     context 'With unknown ID' do
@@ -74,49 +72,40 @@ RSpec.describe Todo do
 
     let(:title) { 'Prepare project' }
     let(:description) { 'Test description' }
-    let(:done) { false }
 
     let :result do
-      todo.create_task title, description: description, done: done
+      todo.create_task title, description: description, done: false
     end
 
     it 'create a new task' do
       expect(result).to be_a(Hash)
-      expect(result['title']).to eq(title)
-      expect(result['description']).to eq(description)
-      expect(result['done']).to eq(false)
-      expect(result['id']).not_to be_nil
+      expect(result[:title]).to eq(title)
+      expect(result[:description]).to eq(description)
+      expect(result[:done]).to eq(false)
+      expect(result[:id]).not_to be_nil
     end
   end
 
   describe '.update_task' do
-    before do
-      storage.write([
-        { 'id' => '123',
-          'title' => 'Call the vet',
-          'description' => 'appointment for vaccines',
-          'done' => false, },
-      ])
-    end
-
     let(:id) { '123' }
 
     it 'edits an existing task' do
-      new_title = 'Call the vet'
-      new_description = 'Checkup on the right paw'
+      new_title = 'Buy book'
+      new_description = 'IT'
+      new_done = true
 
       result = todo.update_task(
         id,
         title: new_title,
         description: new_description,
-        done: true
+        done: new_done
       )
 
       expect(result).to be_a(Hash)
-      expect(result['id']).to eq(id)
-      expect(result['title']).to eq(new_title)
-      expect(result['description']).to eq(new_description)
-      expect(result['done']).to eq(true)
+      expect(result[:id]).to eq(id)
+      expect(result[:title]).to eq(new_title)
+      expect(result[:description]).to eq(new_description)
+      expect(result[:done]).to eq(new_done)
     end
 
     context 'when the task ID does not exist' do
