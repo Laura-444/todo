54fb027eea697b29afe5f9225d728bd168e99f2a
Laura Valladares <lauvagar41012@gmail.com>
2025-06-15 19:35:06 -0600

    fix: add error handling and write validation tests to JSONStorage spec

--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe MemoryStorage do
   let :test_tasks do
     [
-      { id: '2', title: 'hacer una tarea', done: false },
-      { id: '5', title: 'ir de compras', done: false },
+      { id: '2', title: 'buy vegetables', done: false },
+      { id: '5', title: 'buy fruits', done: false },
     ]
   end
 
@@ -32,23 +32,39 @@ RSpec.describe MemoryStorage do
 end
 
 RSpec.describe JSONStorage do
-  let(:file) { 'example_test.json' }
+  let(:file_name) { 'example_test.json' }
+  let(:storage) { JSONStorage.new file_name }
 
-  let :test_tasks do
-    [
-      { id: '2', title: 'drink water', done: false },
-      { id: '5', title: 'read the book', done: true },
-    ]
+  describe '.read' do
+    let(:result) { storage.read }
+
+    context 'with a valid file' do
+      before do
+        JSON.dump([{ id: '1', title: 'something', done: false }], File.open(file_name, 'w'))
+      end
+
+      it 'reads a desired file' do
+        expect(result).to all(be_a(Hash))
+        expect(result.first[:title]).to eq('something')
+      end
+    end
+
+    context 'with invalid JSON file' do
+      before { File.write file_name, 'invalid json {' }
+
+      it 'Return an excepcion' do
+        expect { storage.read }.to raise_error(TodoFileReadError)
+      end
+    end
   end
 
-  let(:storage) { JSONStorage.new file }
+  describe '.write' do
+    let(:result) { storage.write [{ id: '1', title: 'nothing' }] }
 
-  describe 'write and read' do
-    it 'writes and reads the tasks' do
-      storage.write test_tasks
-      result = storage.read
-
-      expect(result).to eq(test_tasks)
+    it 'writes a desired file' do
+      expect(result).to be_a(File)
+      content = JSON.parse File.read(file_name)
+      expect(content.first['title']).to eq('nothing')
     end
   end
 end
