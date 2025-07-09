cd89b9225e442445ae23ce7c65aac9dea277b4cf
Laura Valladares <lauvagar41012@gmail.com>
2025-07-09 09:48:25 -0600

    feat: Organize Todo class structure and refactor delete_task to reuse find_task for better readability

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,108 +1,11 @@
 require 'json'
 require 'csv'
 require 'securerandom'
+require 'pg'
+require 'sequel'
 
-class TodoError < StandardError
-end
-
-class TodoFileReadError < TodoError
-end
-
-class TodoFileWriteError < TodoError
-end
-
-class Storage
-  def read = raise(NotImplementedError, 'Subclasses must implement the read method')
-  def write = raise(NotImplementedError, 'Subclasses must implement the write method')
-end
-
-class CSVStorage < Storage
-  def initialize(file = 'tasks.csv')
-    @file = file
-  end
-
-  def read
-    tasks = []
-    begin
-      CSV.foreach @file, headers: true, header_converters: :symbol do |row|
-        task = row.to_h
-        task[:done] = task[:done] == 'true' if task.key? :done
-        tasks << task
-      end
-      tasks
-    rescue Errno::ENOENT => e
-      raise TodoFileReadError, "File '#{@file}' not found: #{e.message}"
-    rescue Errno::EACCES => e
-      raise TodoFileReadError, "Permission denied to read file '#{@file}': #{e.message}"
-    rescue CSV::MalformedCSVError => e
-      raise TodoFileReadError, "Failed to parse file '#{@file}': invalid CSV format #{e.message}"
-    rescue StandardError => e
-      raise TodoFileReadError, "Unexpected error #{e.message}"
-    end
-  end
-
-  def write(tasks)
-    headers = tasks.first&.keys
-    return if headers.nil?
-
-    begin
-      CSV.open @file, 'w', write_headers: true, headers: headers do |csv|
-        tasks.each do |task|
-          csv << headers.map { |header| task[header] }
-        end
-      end
-    rescue Errno::ENOENT => e
-      raise TodoFileWriteError, "File '#{@file}' not found: #{e.message}"
-    rescue Errno::EACCES => e
-      raise TodoFileWriteError, "Permission denied to write file '#{@file}': #{e.message}"
-    rescue StandardError => e
-      raise TodoFileWriteError, "Unexpected error #{e.message}"
-    end
-  end
-end
-
-class JSONStorage < Storage
-  def initialize(file = 'tasks.json')
-    @file = file
-  end
-
-  def read
-    content_file = File.read @file
-    JSON.parse(content_file, { symbolize_names: true })
-  rescue Errno::ENOENT => e
-    raise TodoFileReadError, "File '#{@file}' not found: #{e.message}"
-  rescue Errno::EACCES => e
-    raise TodoFileReadError, "Permission denied to read file '#{@file}': #{e.message}"
-  rescue JSON::ParserError => e
-    raise TodoFileReadError, "Failed to parse file '#{@file}' invalid Json format: #{e.message}"
-  rescue StandardError => e
-    raise TodoFileReadError, "Unexpected error #{e.message}"
-  end
-
-  def write(tasks)
-    File.write @file, JSON.pretty_generate(tasks)
-  rescue Errno::EACCES => e
-    raise TodoFileWriteError, "Permission denied to write file '#{@file}': #{e.message}"
-  rescue Errno::ENOENT => e
-    raise TodoFileWriteError, "File '#{@file}' not found: #{e.message}"
-  rescue StandardError => e
-    raise TodoFileWriteError, "Unexpected error #{e.message}"
-  end
-end
-
-class MemoryStorage < Storage
-  def initialize(tasks = [])
-    @tasks = tasks.map { |task| task.transform_keys(&:to_sym) }
-  end
-
-  def read
-    @tasks
-  end
-
-  def write(tasks)
-    @tasks = tasks.map { |task| task.transform_keys(&:to_sym) }
-  end
-end
+require_relative 'todo/errors'
+require_relative 'todo/storage'
 
 class Todo
   def initialize(storage)
@@ -119,7 +22,7 @@ class Todo
 
   def delete_task(id)
     tasks = list_tasks
-    deleted_task = tasks.find { |task| task[:id] == id }
+    deleted_task = find_task(id)
 
     return unless deleted_task
 
@@ -133,7 +36,7 @@ class Todo
 
     tasks = list_tasks
 
-    new_task = { id: SecureRandom.uuid, title: title, done: false }.merge attributes
+    new_task = attributes.merge { id: SecureRandom.uuid, title: title}
 
     tasks << new_task
     @storage.write tasks
