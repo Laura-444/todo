7efc8fae72ef1af99279d3de3a2a5a6c801311ad
Laura Valladares <lauvagar41012@gmail.com>
2025-07-16 18:59:06 -0600

    feat: add persistent storage and user validation to Todo core class

--- lib/todo.rb
+++ lib/todo.rb
@@ -6,52 +6,68 @@ require 'sequel'
 
 require_relative 'todo/errors'
 require_relative 'todo/storage'
+require_relative 'todo/repository'
+require_relative 'todo/entities'
 
 class Todo
-  def initialize(storage)
-    @storage = storage
+  attr_reader :user
+
+  def initialize(username, force: false)
+    @user = repository.find_user_by_username username
+
+    return unless user.nil?
+    raise Todo::InvalidUsernameError.new('username no found') unless force
+
+    @user = repository.create_user username
   end
 
   def list_tasks
-    @storage.read
+    repository.list_tasks_by_user_id user.id
   end
 
-  def find_task(id)
-    list_tasks.find { |task| task[:id] == id }
+  def find_task(_id)
+    repository.find_task_by_id
   end
 
   def delete_task(id)
-    tasks = list_tasks
-    deleted_task = find_task(id)
+    task = repository.find_task_by_id id
+    return if task.nil?
 
-    return unless deleted_task
-
-    tasks.delete deleted_task
-    @storage.write tasks
-    deleted_task
+    repository.delete_task_by_id id
   end
 
   def create_task(title, **attributes)
     raise 'Title is required to create a task' if !title.is_a?(String) || title.empty?
 
-    tasks = list_tasks
+    task = {
+      user_id: user.id,
+      title: title,
+      description: attributes.fetch(:description, ''),
+      done: attributes.fetch(:done, false),
+      deadline: attributes[:deadline],
+    }
 
-    new_task = attributes.merge { id: SecureRandom.uuid, title: title}
-
-    tasks << new_task
-    @storage.write tasks
-    new_task
+    repository.create_task task
   end
 
   def edit_task(id, **attributes)
-    tasks = list_tasks
-    index_task_to_edit = tasks.find_index { |task| task[:id] == id }
+    task = repository.find_task_by_id id
+    # index_task_to_edit = tasks.find_index { |task| task.id == id }
+    return if task.nil?
 
-    return if index_task_to_edit.nil?
+    # tasks[index_task_to_edit].merge! attributes.merge id: id
+    repository.edit_user_task_by_id({
+      id: id,
+      title: attributes.fetch(:title, task.title),
+      description: attributes.fetch(:description, task.description),
+      done: attributes.fetch(:done, task.done),
+      deadline: attributes.key?(:deadline) ? attributes[:deadline] : task.deadline,
+    })
+  end
 
-    tasks[index_task_to_edit].merge! attributes.merge id: id
-    @storage.write tasks
+  private
 
-    tasks[index_task_to_edit]
+  def repository
+    @repository ||= Todo::Repository.new
   end
 end
