2dc697f158f80dcb28b991e83130ee9a51daf12d
Laura Valladares <lauvagar41012@gmail.com>
2025-08-01 01:38:33 -0600

    feat: add Project entity to entities module

--- lib/todo/entities.rb
+++ lib/todo/entities.rb
@@ -1,5 +1,7 @@
+require_relative 'entities/entity'
 require_relative 'entities/user'
 require_relative 'entities/task'
+require_relative 'entities/project'
 class Todo
   module Entities
   end
---
+++ lib/todo/entities/project.rb
@@ -0,0 +1,8 @@
+require_relative '../entities/entity'
+
+class Todo
+  module  Entities
+    class Project < Entity
+    end
+  end
+end
