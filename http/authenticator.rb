3bc3e284375f869827f80447e5935f7f06361930
Laura Valladares <lauvagar41012@gmail.com>
2025-08-08 00:53:57 -0600

    feat: add autthenticator.rb

---
+++ http/authenticator.rb
@@ -0,0 +1,22 @@
+require_relative '../lib/todo'
+
+class Authenticator
+  attr_reader :app
+
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    username = env['HTTP_USERNAME']
+    # require 'pry'
+    # binding.pry
+    raise 'Username is required' if username.nil?
+
+    app.todo = Todo.new username
+
+    app.call env
+  rescue Todo::InvalidUsernameError
+    raise "Username doesn't exist"
+  end
+end
